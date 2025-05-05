7a27d67a7aa71f8aa097800e00cdc6765390b258
valonea2 <nestori.valonen@gmail.com>
2025-05-05 16:24:40 +0300

    setuppi

---
+++ lopputyo/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import AboutPage from "./pages/AboutPage"
+import FeedPage from "./pages/FeedPage"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<AboutPage />} />
+        <Route path="/feed" element={<FeedPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ lopputyo/src/pages/AboutPage.jsx
@@ -0,0 +1,10 @@
+import { Text, Heading, Link } from "@chakra-ui/react"
+
+
+export default function AboutPage() {
+    return(
+    <>
+        <Heading as="h1">Tähän tulee about-sivu</Heading>
+    </>
+    )
+}

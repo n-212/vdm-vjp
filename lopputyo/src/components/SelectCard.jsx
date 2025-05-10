fdfd9eaa31a1b94d9e5afc4ba7ad28f039323a73
valonea2 <nestori.valonen@gmail.com>
2025-05-10 17:47:35 +0300

    bubble item

--- lopputyo/src/components/SelectCard.jsx
+++ lopputyo/src/components/SelectCard.jsx
@@ -1,19 +1,23 @@
-import { Card, CardHeader, CardBody, Box, Image, Heading, Text, Flex } from '@chakra-ui/react'
+import { Card, CardBody, Box, Image, Heading, Text, Flex } from '@chakra-ui/react'
 
 
-export default function SelectCard() {
+
+
+export default function SelectCard({ bubble }) {
     return(
         <Card backgroundColor="kuplassa.kupla1" borderRadius={"15px"} p={"8px"} aspectRatio={"1 / 1"} m="30px">
             <Flex justifyContent={"center"}>
-                <Box backgroundColor="black" borderRadius={"15px"} padding="15px" width="fit-content" m="8px">
-                    <Heading fontFamily="mono" fontWeight={"500"} fontSize={"35px"} textTransform={"uppercase"} color="white" textAlign={"center"} px={"12px"}>salaliitto</Heading>
+                <Box backgroundColor={bubble.headingBackgroundColor} borderRadius={"15px"} padding="15px" width="fit-content" m="8px">
+                    <Heading fontFamily="mono" fontWeight={"500"} fontSize={"35px"} textTransform={"uppercase"} color={bubble.headingTextColor} textAlign={"center"} px={"12px"}>
+                        {bubble.heading}
+                    </Heading>
                 </Box>
             </Flex>
             <Flex justifyContent={"center"}>
-                <Image boxSize='100px' src="/hahmo.png" m="8px"/>
+                <Image boxSize='100px' src={bubble.avatarSrc} m="8px"/>
             </Flex>
             <Text mx="8px" fontSize="14px">
-                Herää! Leo on nähnyt läpi verhon: maailmaa pyörittävät salaiset eliitit, algoritmit aivopesevät massoja, ja oikeat totuudet sensuroidaan. 
+                {bubble.description}
             </Text>
         </Card>
     )

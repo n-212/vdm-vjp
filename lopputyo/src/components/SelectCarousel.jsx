c350f8904c44c9993063045f6b2184d16edffb10
valonea2 <nestori.valonen@gmail.com>
2025-05-11 22:02:36 +0300

    lataus jsonista

--- lopputyo/src/components/SelectCarousel.jsx
+++ lopputyo/src/components/SelectCarousel.jsx
@@ -1,41 +1,7 @@
 import SelectCard from "./SelectCard";
 import { Provider, Carousel, LeftButton, RightButton } from "chakra-ui-carousel"
 import { Box, Button, Stack } from "@chakra-ui/react"
-
-const data = {
-  heading: "salaliitto",
-  headingBackgroundColor: "black",
-  headingColor: "white",
-  avatarSrc: "/hahmo.png",
-  description: "Herää! Media valehtelee. ...",
-};
-
-
-const data1 = {
-    heading: "ylikulutus",
-    headingBackgroundColor: "black",
-    headingColor: "white",
-    avatarSrc: "/hahmo.png",
-    description: "Herää! Media valehtelee. ...",
-  };
-
-const data2 = {
-heading: "konservatiivi",
-headingBackgroundColor: "black",
-headingColor: "white",
-avatarSrc: "/hahmo.png",
-description: "Herää! Media valehtelee. ...",
-};
-
-const data3 = {
-    heading: "hyvinvointi",
-    headingBackgroundColor: "black",
-    headingColor: "white",
-    avatarSrc: "/hahmo.png",
-    description: "Herää! Media valehtelee. ...",
-  };
-
-const bubbles = [data, data1, data3, data2]
+import feeds from "../feeds.json"
 
 
 export default function SelectCarousel() {
@@ -49,7 +15,7 @@ export default function SelectCarousel() {
         loop={false}
         // You can add more props as needed for customization
         >
-        {bubbles.map((bubble) => (
+        {feeds.map((bubble) => (
             <>
             <Stack>
                 <SelectCard key={bubble.heading} data={bubble} />
